fab65addb44d1f520a21359017aa96bf524814c5
José Manuel Lucas <josemanuel@jmlweb.es>
2019-03-16 00:47:19 +0100

    More docs

--- src/mapIndexed.js
+++ src/mapIndexed.js
@@ -1,5 +1,16 @@
 import { addIndex, map } from 'ramda';
 
+/**
+ * Indexed version of map
+ *
+ * @function
+ * @param {Function} fn - The function passed to map, receiving index as second argument
+ * @param {Array} arr - Source array
+ * @returns {Array}
+ *
+ * @example
+ * mapIndexed((val, idx) => `${idx}.${val}`, ['a', 'b', 'c']); // ['0.a', '1.b', '2.c']
+ */
 const mapIndexed = addIndex(map);
 
 export default mapIndexed;
--- src/notEndsWith.js
+++ src/notEndsWith.js
@@ -2,6 +2,17 @@ import { complement } from 'ramda';
 
 import endsWith from './endsWith';
 
+/**
+ * Complement for endsWith
+ *
+ * @function
+ * @param {*} val - The value provided
+ * @returns {Boolean}
+ *
+ * @example
+ * isNotNil('a'); // true
+ * isNotNil(undefined); // false
+ */
 const notEndsWith = complement(endsWith);
 
 export default notEndsWith;
